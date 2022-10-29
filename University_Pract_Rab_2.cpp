92ad306b6ce1b10f1ac434b744b49664f659ecb8
Ivaca <insurgentgamestv@gmail.com>
2022-10-29 16:55:31 +0300

    first version

---
+++ University_Pract_Rab_2.cpp
@@ -0,0 +1,34 @@
+// Начало программы
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int array[100]; // инициализация массива
+    int number; // переменная для дальнейшего определения того, что будет выполняться
+
+    while (true){
+        cout << "Введите номер элемента для выполнения: ";
+        cin >> number;
+        switch(number) {
+            // ЗАДАНИЕ 1
+            case 1: 
+                // Алгоритм создания массива
+                for(int i = 0; i <=99; i++){
+                    array[i] = rand() % 199 - 99;
+                }
+                // Вывод массива в консоль
+                for(int i = 0; i <=99; i++){
+                    cout << array[i] << " ";
+                }
+                cout << endl;
+                break;
+            default:
+                break;
+            }
+    }
+}
