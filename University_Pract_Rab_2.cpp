d57c2c9e755fb9380ad556d5f61639bf973b560c
Ivaca <insurgentgamestv@gmail.com>
2022-10-30 16:44:08 +0300

    Добавлен выбор номера функции через массив + косметические изменения в коде

--- University_Pract_Rab_2.cpp
+++ University_Pract_Rab_2.cpp
@@ -1,5 +1,6 @@
 // Начало программы
 
+#include <string>
 #include <iostream>
 #include <chrono>
 #include <ctime>
@@ -15,12 +16,14 @@ int main() {
     cin >> count;
 
     int array[count]; // инициализация массива
+    int instructions[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}; // набор инструкций программы
     int min; // минимальное число
     int max; // максимальное число
-    char number; // переменная для дальнейшего определения того, что будет выполняться
+    int number; // переменная для дальнейшего определения того, что будет выполняться
     int more; // Число, больше которого должно быть в case 
     int less; // Число, меньше которого должно быть в case 
 
+    // Переменыые для нахождения времени
     auto start = steady_clock::now();
     auto end = steady_clock::now();
     auto result = duration_cast<nanoseconds>(end - start);
@@ -29,10 +32,20 @@ int main() {
         cout << "Введите номер элемента для выполнения: ";
         cin >> number;
         cout << endl;
-        switch(number) {
+
+        // Выход из программы
+        if(number == 0)
+            break;
+
+        number -= 1; // для индекса
+        
+        switch(instructions[number]){
+
             // ЗАДАНИЕ 1
-            case 'a': 
+            case 12: 
+                // работа с промежутком времени
                 start = steady_clock::now();
+
                 // Алгоритм создания массива
                 for(int i = 0; i <=count-1; i++){
                     array[i] = rand() % 199 - 99;
@@ -43,31 +56,40 @@ int main() {
                 }
                 cout << endl;
 
+                // работа с промежутком времени + вывод времени
                 end = steady_clock::now();
                 result = duration_cast<nanoseconds>(end - start);
                 cout << result.count() << " nanoseconds" << endl;
+
                 break;
+
+            // ЗАДАНИЕ 2.1 (Bubble Sort)
+            case 1:
+                break;
+
             // ЗАДАНИЕ 3 (максимальный и минимальный элементы массива)
-            case '6':
+            case 6:
                 min = 0;
                 max = 0;
+
                 for(int i = 0; i <= count-1; i++){
+                    // Нахождение минимального элемента
                     if(array[i] < min){
                         min = array[i];
                         continue;
                     }
+                    // Нахождение максимального элемента
                     if(array[i] > max){
                         max = array[i];
                         continue;
                     }
                 }
+                // вывод минимального и максимального элемента
                 cout << "Минимальный элемент: " 
                 << min << ", максимальный элемент: " << max << endl;
+            
             default:
                 break;
         }
-
-        if(number == '0')
-            break;
     }
 }
