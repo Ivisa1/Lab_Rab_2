95a88cc3d18bcb38635843a2e232dc853dbc65cf
Ivaca <insurgentgamestv@gmail.com>
2022-10-30 15:31:13 +0300

    Добавлен подсчёт времени для создания массива

--- University_Pract_Rab_2.cpp
+++ University_Pract_Rab_2.cpp
@@ -5,11 +5,16 @@
 #include <ctime>
 
 using namespace std;
+using namespace chrono;
 
 int main() {
     setlocale(0, "");
     int array[100]; // инициализация массива
     char number; // переменная для дальнейшего определения того, что будет выполняться
+    
+    auto start = steady_clock::now();
+    auto end = steady_clock::now();
+    auto result = duration_cast<nanoseconds>(end - start);
 
     while (true){
         cout << "Введите номер элемента для выполнения: ";
@@ -18,6 +23,7 @@ int main() {
         switch(number) {
             // ЗАДАНИЕ 1
             case '1': 
+                start = steady_clock::now();
                 // Алгоритм создания массива
                 for(int i = 0; i <=99; i++){
                     array[i] = rand() % 199 - 99;
@@ -27,10 +33,15 @@ int main() {
                     cout << array[i] << " ";
                 }
                 cout << endl;
+
+                end = steady_clock::now();
+                result = duration_cast<nanoseconds>(end - start);
+                cout << result.count() << " nanoseconds" << endl;
                 break;
             default:
                 break;
-            }
+        }
+
         if(number == '0')
             break;
     }
